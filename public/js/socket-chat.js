9048631185608c313e05120bbaf4ec6af0548ded
Nemo <user@Mackie-Bockie.local>
2020-04-06 19:39:26 -0400

    Primer Commit

---
+++ public/js/socket-chat.js
@@ -0,0 +1,56 @@
+var socket = io();
+
+var params = new URLSearchParams(window.location.search);
+
+if (!params.has("nombre") || !params.has("sala")) {
+    window.location = "index.html";
+    throw new Error("El nombre y la sala es necesario");
+}
+
+var usuario = {
+    nombre: params.get('nombre'),
+    sala: params.get('sala')
+};
+
+socket.on('connect', function() {
+
+    socket.emit('entrarChat', usuario, function(resp) {
+        console.log('Usuarios conectados', resp);
+    })
+
+});
+
+// escuchar
+socket.on('disconnect', function() {
+
+    console.log('Perdimos conexión con el servidor');
+
+});
+
+
+// Enviar información
+// socket.emit('crearMensaje', {
+//     usuario: 'Fernando',
+//     mensaje: 'Hola Mundo'
+// }, function(resp) {
+//     console.log('respuesta server: ', resp);
+// });
+
+// Escuchar información
+socket.on('crearMensaje', function(mensaje) {
+
+    console.log('Servidor:', mensaje);
+
+});
+
+
+/* Cuando usuariuo entra o sale del chat */
+socket.on('listaPersona', function(personas) {
+
+    console.log(personas);
+
+});
+
+
+/* mensajes privados */
+socket.on('mensajePrivado', function(mensaje) {});
