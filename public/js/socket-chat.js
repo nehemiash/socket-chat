c59389cba187597fe599971518951fbe2fd23270
Nemo <user@Mackie-Bockie.local>
2020-04-06 22:37:57 -0400

    Integracion frontend

--- public/js/socket-chat.js
+++ public/js/socket-chat.js
@@ -2,9 +2,9 @@ var socket = io();
 
 var params = new URLSearchParams(window.location.search);
 
-if (!params.has("nombre") || !params.has("sala")) {
-    window.location = "index.html";
-    throw new Error("El nombre y la sala es necesario");
+if (!params.has('nombre') || !params.has('sala')) {
+    window.location = 'index.html';
+    throw new Error('El nombre y sala son necesarios');
 }
 
 var usuario = {
@@ -12,11 +12,14 @@ var usuario = {
     sala: params.get('sala')
 };
 
+
+
 socket.on('connect', function() {
+    console.log('Conectado al servidor');
 
     socket.emit('entrarChat', usuario, function(resp) {
-        console.log('Usuarios conectados', resp);
-    })
+        renderizarUsuarios(resp);
+    });
 
 });
 
@@ -30,7 +33,7 @@ socket.on('disconnect', function() {
 
 // Enviar información
 // socket.emit('crearMensaje', {
-//     usuario: 'Fernando',
+//     nombre: 'Fernando',
 //     mensaje: 'Hola Mundo'
 // }, function(resp) {
 //     console.log('respuesta server: ', resp);
@@ -38,19 +41,20 @@ socket.on('disconnect', function() {
 
 // Escuchar información
 socket.on('crearMensaje', function(mensaje) {
-
-    console.log('Servidor:', mensaje);
-
+    // console.log('Servidor:', mensaje);
+    renderizarMensajes(mensaje, false);
+    scrollBottom();
 });
 
-
-/* Cuando usuariuo entra o sale del chat */
+// Escuchar cambios de usuarios
+// cuando un usuario entra o sale del chat
 socket.on('listaPersona', function(personas) {
-
-    console.log(personas);
-
+    renderizarUsuarios(personas);
 });
 
+// Mensajes privados
+socket.on('mensajePrivado', function(mensaje) {
 
-/* mensajes privados */
-socket.on('mensajePrivado', function(mensaje) {});
+    console.log('Mensaje Privado:', mensaje);
+
+});
